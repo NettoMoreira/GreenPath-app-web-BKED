c14fbcf6dc8508f5b60181521d08d315a483aeb0
neto_ <neto_@NETTO-MOREIRA>
2023-05-03 20:14:35 -0300

    Adicionar arquivos de projeto.

---
+++ GreenPath-app-web-BKED/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreenPath_app_web_BKED.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
